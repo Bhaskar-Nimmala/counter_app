2ac106ecf3bb4fa3acdf6139fdd86dc7a9c3f8c6
Bhaskar Satya Krishna Nimmala <bsnimmala@nisum.com>
2024-03-11 11:56:23 +0530

    Updated count logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
   return (
-    <></>
+    <div style={{ display: 'flex', flexDirection: 'column', rowGap: '10px' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}> COUNTER : {count}</div>
+      <div style={{ display: 'flex', justifyContent: 'center', columnGap: '10px' }}>
+        <button type="button" onClick={() => setCount(count + 1)} > +1 </button>
+        <button type="button" onClick={() => setCount(count - 1)}> -1 </button>
+      </div>
+    </div>
   );
 }
 
